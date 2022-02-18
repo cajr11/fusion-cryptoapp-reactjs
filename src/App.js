0aa2df179041e1e7ac56235066d67ffd445edc22
cajr11 <caondo@yahoo.co.uk>
2022-02-17 23:53:26 -0600

    homepage complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-gray-600">
       <Layout>
         <Routes>
             <Route path="/" element={<Home />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
         <Poster />
         <BtcUsdMainChart />
       </div>
-      <div className="w-11/12 mx-auto mt-2 md:h-60 md:mt-5 bg-neutral-900">
-          <h1>Top 10 Coins</h1>
+      <div className="w-11/12 md:carousel-coins mx-auto mt-2 h-64 md:h-64 bg-neutral-900 flex flex-col justify-center rounded-md">
+          <h1 className='mt-3 mb-3 text-center text-slate-300 font-bold'>Top 10 Coins</h1>
           <CarouselTopTen />
       </div>
     </div>
