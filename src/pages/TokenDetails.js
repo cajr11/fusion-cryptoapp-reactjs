6f41f98eb4478617a90c1156a22ed56c64fc45a1
cajr11 <caondo@yahoo.co.uk>
2022-02-20 16:47:00 -0600

    token details page complete

--- src/pages/TokenDetails.js
+++ src/pages/TokenDetails.js
@@ -3,7 +3,7 @@ import TokenChart from '../components/token-details/TokenChart'
 import TokenHeading from '../components/token-details/TokenHeading'
 import TokenStats from '../components/token-details/TokenStats'
 import { useParams } from 'react-router-dom';
-
+import { Rings } from 'react-loader-spinner';
 
 const TokenDetails = () => {
     const params = useParams();
@@ -30,6 +30,15 @@ const TokenDetails = () => {
 
   return (
     <div className='w-11/12 relative mx-auto mt-4'>
+
+        {/* Loading Ring */}
+        {!tokenData &&  (
+          <div  className="w-full mx-auto flex h-full justify-center items-center">
+            <Rings color="rgb(217, 119, 6)" height={200} width={200}/>
+          </div>
+          )}
+
+
         {/* Token Heading and Price */}
         {tokenData && <TokenHeading price={tokenData.price} change={tokenData.change} icon={tokenData.iconUrl} name={tokenData.name} symbol={tokenData.symbol}/>}
 
@@ -38,6 +47,16 @@ const TokenDetails = () => {
 
         {/* Token General Stats */}
         {tokenData && <TokenStats market={tokenData.marketCap} btcPrice={tokenData.btcPrice} volume={tokenData["24hVolume"]} rank={tokenData.rank} website={tokenData.websiteUrl} whitepaper={tokenData.links[1].url} />}
+
+        {/* Token Description */}
+        {tokenData && (
+          <div className='text-2xl text-center border text-stone-300 mt-5 px-6 py-4 mb-10 bg-neutral-900 border-gray-600 rounded-md'>
+            <div className='underline mb-4'>What is {tokenData.name} ?</div>
+            <div className='text-center token-description text-base' dangerouslySetInnerHTML={{__html: `${tokenData && tokenData.description}`}}>
+            </div>
+          </div>
+          
+          )}
     </div>
   )
 }
