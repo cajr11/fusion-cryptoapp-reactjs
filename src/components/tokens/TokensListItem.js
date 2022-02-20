794138714ef1658e8d7cd20d7019ec0ec5fe1bec
cajr11 <caondo@yahoo.co.uk>
2022-02-19 18:40:46 -0600

    set color change for negative list 24h%

--- src/components/tokens/TokensListItem.js
+++ src/components/tokens/TokensListItem.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CurrencyFormat from 'react-currency-format';
 
 const TokensListItem = ({ name, rank, volume, market, icon, price, change, symbol}) => {
-    const [isDown, setIsDown] = useState(null);
+    const isNegative = parseFloat(change) < 0 ? true : false;
 
   return (
     <div className='h-16 border border-x-0 border-t-0 border-b-gray-600 flex items-center justify-center text-stone-300 tokenlist-item cursor-pointer'>
@@ -17,7 +17,7 @@ const TokensListItem = ({ name, rank, volume, market, icon, price, change, symbo
         <div className='h-full flex justify-center items-center basis-2/12'>
             <CurrencyFormat value={parseFloat(price).toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />
         </div>
-        <div className='h-full flex justify-center items-center basis-1/12'>1024%</div>
+        <div className={`${!isNegative ? "text-green-600" : "text-red-600"} h-full flex justify-center items-center basis-1/12`}>{change.split("-").join("")}%</div>
         <div className='h-full flex justify-center items-center basis-3/12'>
             <CurrencyFormat value={market} displayType={'text'} thousandSeparator={true} prefix={'$'} />
         </div>
