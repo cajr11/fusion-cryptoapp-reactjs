ecb558fce02efee557babc9ca8c4bbe5813c8f57
cajr11 <caondo@yahoo.co.uk>
2022-02-22 15:08:58 -0600

    chain network selector tab

---
+++ src/components/transfer/BscNetworks.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const BscNetworks = () => {
+    const [mainNetActive, setMainNetActive] = useState(true);
+    const [testNetActive, setTestNetActive] = useState(false);
+
+    const mainNetActiveHandler = () => {
+        setTestNetActive(false)
+        setMainNetActive(true);
+    }
+
+    const testNetActiveHandler = () => {
+        setMainNetActive(false);
+        setTestNetActive(true)
+    }
+
+  return (
+    <div className='w-40 md:w-52 h-full flex items-center justify-evenly'>
+        <input type="radio" name="chain" value="Ethereum" className='hidden'/>
+        <label for="ethereum" className={`${mainNetActive ? 'bg-gray-800' : ''} text-yellow-600 border w-1/2 text-center h-full flex items-center justify-center cursor-pointer`} onClick={mainNetActiveHandler}>Mainnet</label>
+
+        <input type="radio" name="chain" value="BSC" className='hidden'/>
+        <label for="BSC" className={`${testNetActive ? 'bg-gray-800' : ''} text-yellow-600 border w-1/2 text-center h-full flex items-center justify-center cursor-pointer`} onClick={testNetActiveHandler}>Testnet</label>
+    </div>
+  )
+}
+
+export default BscNetworks
--- src/components/transfer/Chain.js
+++ src/components/transfer/Chain.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import BscNetworks from './BscNetworks';
+import EthereumNetworks from './EthereumNetworks';
 
 const Chain = () => {
     const [ethActive, setEthActive] = useState(true);
@@ -16,13 +18,25 @@ const Chain = () => {
 
   return (
     <div className='w-11/12 mx-auto mt-10 flex items-center justify-center h-12'>
-        <div className='w-52 h-full flex items-center justify-evenly'>
-            <input type="radio" name="chain" value="Ethereum" className='hidden'/>
-            <label for="ethereum" className={`${ethActive ? 'bg-gray-800' : ''} text-yellow-600 border w-1/2 text-center h-full flex items-center justify-center cursor-pointer`} onClick={ethSelectedHandler}>Ethereum</label>
 
-            <input type="radio" name="chain" value="BSC" className='hidden'/>
-            <label for="BSC" className={`${bscActive ? 'bg-gray-800' : ''} text-yellow-600 border w-1/2 text-center h-full flex items-center justify-center cursor-pointer`} onClick={bscSelectedHandler}>BSC</label>
-        </div>
+       <div className='w-full h-full flex items-center justify-between md:w-4/6'>
+
+            {/* Chains */}
+            <div className='w-40 md:w-52 h-full flex items-center justify-evenly'>
+                <input type="radio" name="chain" value="Ethereum" className='hidden'/>
+                <label for="ethereum" className={`${ethActive ? 'bg-gray-800' : ''} text-yellow-600 border w-1/2 text-center h-full flex items-center justify-center cursor-pointer`} onClick={ethSelectedHandler}>Ethereum</label>
+
+                <input type="radio" name="chain" value="BSC" className='hidden'/>
+                <label for="BSC" className={`${bscActive ? 'bg-gray-800' : ''} text-yellow-600 border w-1/2 text-center h-full flex items-center justify-center cursor-pointer`} onClick={bscSelectedHandler}>BSC</label>
+            </div>
+
+
+            {/* Networks */}
+            {ethActive && <EthereumNetworks />}
+            {bscActive && <BscNetworks />}
+
+       </div>
+
     </div>
   )
 }
