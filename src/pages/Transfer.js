ade5693d61381c64077f3a2a619e7fa1ebe8a0a2
cajr11 <caondo@yahoo.co.uk>
2022-02-23 00:59:48 -0600

    address, amount and send payment handlers in progress

--- src/pages/Transfer.js
+++ src/pages/Transfer.js
@@ -1,9 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Chain from '../components/transfer/Chain'
 import AuthContext from '../store/auth-context';
+import { ethers } from "ethers";
 
 const Transfer = () => {
+    const [amount, setAmount] = useState(0)
+    const [destinationAddress, setDestinationAddress] = useState("")
     const ctx = useContext(AuthContext)
+
+    const destinationAddressHandler = (e) => {
+        setDestinationAddress(e.target.value);
+    }
+
+    const amountHandler = (e) => {
+        setAmount(parseFloat(e.target.value));
+    }
+
+    const initiatePaymentHandler = async () => {
+        console.log({amount, destinationAddress});
+    }
 
 
     if (!ctx.isLoggedIn) {
@@ -22,9 +37,9 @@ const Transfer = () => {
                 {/* Tranfer Form */}
                 <form className='mt-3 w-11/12 lg:w-9/12 h-96 mx-auto border-2 border-gray-600 bg-neutral-900 flex items-center justify-center rounded-md'>
                     <div className='h-4/6 w-9/12 flex flex-col items-center justify-evenly border-2 border-gray-600'>
-                        <input className='w-8/12 h-1/6 rounded-md placeholder-neutral-600 pl-3' type="number" step="0.1" placeholder='Amount' />
-                        <input className='w-8/12 h-1/6 rounded-md placeholder-neutral-600 pl-3' type="text" minLength="42" maxLength="42" placeholder='Address' />
-                        <button className='h-14 w-28 bg-gray-600 rounded-md font-bold text-stone-300'>Send</button>
+                        <input className='w-8/12 h-1/6 rounded-md placeholder-neutral-600 pl-3' type="number" step="0.1" placeholder='Amount' value={amount} onChange={amountHandler} />
+                        <input className='w-8/12 h-1/6 rounded-md placeholder-neutral-600 pl-3' type="text" minLength="42" maxLength="42" placeholder='Address' value={destinationAddress} onChange={destinationAddressHandler} />
+                        <button className='h-14 w-28 bg-gray-600 rounded-md font-bold text-stone-300' onClick={initiatePaymentHandler}>Send</button>
                     </div>
                 </form>
             </div>
